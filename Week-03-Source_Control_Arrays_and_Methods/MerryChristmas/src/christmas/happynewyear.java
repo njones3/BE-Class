4f36001735353acaa0064b1efc9d3947f8c40fa8
njones3 <njones3@hotmail.com>
2023-12-21 19:17:21 -0500

    add pushing files

---
+++ Week-03-Source_Control_Arrays_and_Methods/MerryChristmas/src/christmas/happynewyear.java
@@ -0,0 +1,11 @@
+package christmas;
+
+public class happynewyear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Merry Christmas and Happy New Year!!");
+		
+	}
+
+}
