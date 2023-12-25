e2a6fc605f4b4a47994757149a2b4f39221abf41
njones3 <njones3@hotmail.com>
2023-12-25 17:04:49 -0500

    Codding Challenge

---
+++ Week-04-Collections/Collections/backwards.java
@@ -0,0 +1,28 @@
+package CodingChallenge;
+
+public class backwards {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Washington";
+		int countLetters = 0;
+				
+		System.out.println(str);	
+		
+		
+		for (int i = 0; i < str.length(); i++) {
+			if (i < str.length(
+					));
+			countLetters++;
+		}
+		System.out.println(countLetters);
+		
+		for (int i = str.length(); i > 0; i--) {
+			System.out.print(str.charAt(i - 1));
+		}
+		
+	}	
+	
+	
+
+}
